e7b696d9510c24159091ec9fa23c417fb52289e6
Ivana Kuzmanoska <ivanakuzmanoska@yahoo.com>
2018-10-10 22:08:56 +0200

    pojasnuvanje

--- NodeJS/index.js
+++ NodeJS/index.js
@@ -25,18 +25,25 @@ mongo.Init();
 
 var app = express();
 app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', root);
 
 app.post('/login', auth.login);
-app.get('/logout', auth.logout);
+app.get('/logout', auth.logout);    //logout(req,res);
 
 app.get('/users', users.getAllUsers);
-app.get('/users/name/:name',users.getUserByName); //ekspresot primetuva deka ima takvo url , stom nekoj odi na toa url togas povikaj mi ja taa funkcija. taa funkicja se naga vo var userss koja e def gore. i ke oide vo filot users koj se naoga vo handlers .
+app.get('/users/name/:name', users.getUserByName);
 app.get('/users/:id', users.getUserById);
 app.post('/users', users.createUser);
 app.delete('/users/:id', users.deleteById);
 app.put('/users/:id', users.updateById);
+
+app.post('/cv', cv.createCV);
+app.put('/cv/:id', cv.updateCVById);
+app.delete('/cv/:id', cv.deleteCVById);
+app.get('/cv', cv.getAllCVs);
+app.get('/cv/:id', cv.getCVById);
 
 
 app.listen(8080);
