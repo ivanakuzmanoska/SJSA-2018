b7952009dbea7fdf74a5d84dbd22898bbf168472
Ivana Kuzmanoska <ivanakuzmanoska@yahoo.com>
2018-10-22 19:48:19 +0200

    validation

--- NodeJS/handlers/users.js
+++ NodeJS/handlers/users.js
@@ -1,6 +1,7 @@
-
+var utils = require("../utils");
 var users = require("../models/users");
 var bcrypt = require("bcrypt");
+var validator = require("fastest-validator");
 
 var getAllUsers = (req, res) => {
     users.getAllUsers((err, data) => {
@@ -39,28 +40,53 @@ var getUserById = (req, res) => {
 }
 
 var createUser = (req, res) => {
-    var valid = req.body.firstname != undefined && req.body.firstname != ""
-                 &&  req.body.lastname != undefined && req.body.lastname != ""
-                 &&  req.body.email != undefined && req.body.email != ""
-                 &&  req.body.password != undefined && req.body.password != ""
-     if(valid) {
-         bcrypt.hash(req.body.password, 10, (err, hash) =>{
-             var userData = req.body;
-             userData.password = hash;
-             userData.role = 'user';
-             users.createUser(req.body, (err) => {
-             if(err){
-            res.send(err);
-             } else {
-              res.status(201).send("OK");
-             }
-         });
-     });
-    
+    // var valid = req.body.firstname != undefined && req.body.firstname != ""
+    //             && req.body.lastname != undefined && req.body.lastname != ""
+    //             && req.body.email != undefined && req.body.email != ""
+    //             && req.body.password != undefined && req.body.password != "";
+    // var valid = !utils.isEmpty(req.body.firstname) 
+    //             && !utils.isEmpty(req.body.lastname) 
+    //             && !utils.isEmpty(req.body.email) 
+    //             && !utils.isEmpty(req.body.password);
+
+    var schema = {
+        firstname: {type: 'string', empty: false},
+        lastname: {type: 'string', empty: false},
+        email: {type: 'email', empty: false},
+        password: {type: 'string', min: 8, max: 16, empty: false}
+    }
+
+    let v = new validator();
+    var valid = v.validate(req.body, schema);
+
+    if(valid === true) {
+        users.getUserByEmail(req.body.email, (err, data) => {
+            if (err) {
+                return res.send(err);
+            } else {
+                if (data == null) {
+                    bcrypt.hash(req.body.password, 10, (err, hash) => {
+                        var userData = req.body;
+                        userData.password = hash;
+                        userData.role = 'user';
+                        users.createUser(userData, (err) => {
+                            if(err) {
+                                res.send(err);
+                            } else {
+                                res.status(201).send("OK");
+                            }
+                        });
+                    });
+                } else {
+                    res.status(400).send("User exists");
+                }
+            }
+        })
     } else {
-          res.status(400).send('Bad request')
+        res.status(400).send(valid);
     }
 }
+
 
 var deleteById = (req, res) => {
     var id = req.params.id;
