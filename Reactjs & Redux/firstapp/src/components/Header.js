8822f5c529c152ed4156f298044216e821c626d9
Ivana Kuzmanoska <ivanakuzmanoska@yahoo.com>
2018-10-26 21:53:52 +0200

    React

---
+++ Reactjs & Redux/firstapp/src/components/Header.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+export const Header = () => {
+    return (
+        <header>
+            <nav>
+                <ul>
+                    <li> 
+                         <a href="a">Home</a> 
+                     </li>
+                     <li> 
+                         <a href="a">About React</a> 
+                     </li>
+                     <li> 
+                         <a href="a">Tutorials</a> 
+                     </li>
+                     <li> 
+                         <a href="a">Contact</a> 
+                     </li>
+
+                  </ul>
+             </nav>
+        </header>
+    )
+}
